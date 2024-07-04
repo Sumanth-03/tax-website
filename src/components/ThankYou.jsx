08aac1ab576333fde2e0c3b9bf4653311023a37e
Sumanth G R <sumanth@snowtint.com>
2024-07-04 17:04:18 +0530

    made few changes

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import img from '../Assets/background.png'
+import logo from '../Assets/Logo.png'
+import Image1 from '../Assets/Thank you.png'
+
 const ThankYou = () => {
   return (
-    <div className='m-2 mt-4 text-center'>
-      <img src={img} alt='Thank you'></img>
-    </div>
+    <>
+        <header className="d-flex align-items-center p-3 bg-dark text-white">
+            <img src={logo} alt="Logo" className="logo me-3" />
+        </header>
+        <section className="text-center">
+            <img src={Image1} alt="header-image" className="img-fluid w-100" 
+                style={{ maxHeight: '60vh', maxWidth: '1000px' }}
+            />
+        </section>
+        <div className='m-2 mt-4 text-center'>
+            <img src={img} alt='Thank you'></img>
+        </div>
+    </>
+    
   );
 };
 
